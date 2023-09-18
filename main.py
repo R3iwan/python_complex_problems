bbe1983e242593aab5db87290154255e05f8c5d3
R3iwan <r3iwan.study@gmail.com>
2023-09-18 11:18:12 +0600

    added problem 14

--- main.py
+++ main.py
@@ -52,7 +52,24 @@ match choice:
     case 4:
         print("Invalid number")
 
-# Problem 14: File Manipulation
+# Problem 14: Web Scarper
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.w3schools.com/"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    links = soup.find_all("a")
+
+    for link in links:
+        print(link.get("href"))
+
+else:
+    print("Failed to retrieve the web page.")
 
 
 
